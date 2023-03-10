d93f2d25c1238f4648e04ec9ee7687c85a249a41
BollgadeL <Bollgade@yandex.ru>
2023-03-10 20:41:13 +0300

    route settings for post

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,3 +1,4 @@
+import { Post } from 'entities/Post/ui/Post';
 import { MainPage } from 'pages/MainPage';
 import { NotFoundPage } from 'pages/NotFoundPage';
 import React from 'react';
@@ -5,8 +6,8 @@ import { RouteProps } from 'react-router-dom';
 import MainIcon from 'shared/assets/icons/main-20-20.svg';
 
 export enum AppRoutes {
-  MAIN = 'main',
-
+  ALL_POSTS = 'posts',
+  POST = 'post',
   // last-one
   NOT_FOUND = 'not_found',
 }
@@ -21,13 +22,17 @@ export type RouteItem = RouteProps & {
 }
 
 export const RouteElements: Record<AppRoutes, RouteItem> = {
-  [AppRoutes.MAIN]: {
+  [AppRoutes.ALL_POSTS]: {
     path: '/',
     element: <MainPage />,
     link: {
       text: 'Main',
       Icon: MainIcon,
     },
+  },
+  [AppRoutes.POST]: {
+    path: '/posts/:id',
+    element: <Post />,
   },
 
   // last-one
